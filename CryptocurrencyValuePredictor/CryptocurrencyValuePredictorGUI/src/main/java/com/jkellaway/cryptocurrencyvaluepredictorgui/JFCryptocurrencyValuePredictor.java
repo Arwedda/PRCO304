52b284797848917e5af505b6ebb4dc9881e9029f
Joseph Kellaway <joseph.kellaway@students.plymouth.ac.uk>
2018-03-13 22:52:42 +0000

    Added contact information & relevant actions when they are clicked.

--- CryptocurrencyValuePredictor/CryptocurrencyValuePredictorGUI/src/main/java/com/jkellaway/cryptocurrencyvaluepredictorgui/JFCryptocurrencyValuePredictor.java
+++ CryptocurrencyValuePredictor/CryptocurrencyValuePredictorGUI/src/main/java/com/jkellaway/cryptocurrencyvaluepredictorgui/JFCryptocurrencyValuePredictor.java
@@ -5,6 +5,12 @@ import com.jkellaway.cryptocurrencyvaluepredictorlibrary.model.Currency;
 import com.jkellaway.cryptocurrencyvaluepredictorlibrary.model.ExchangeRate;
 import com.jkellaway.cryptocurrencyvaluepredictorlibrary.valuepredictor.CryptocurrencyValuePredictor;
 import com.jkellaway.guihelpers.TableConfigurer;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
@@ -80,6 +86,10 @@ public class JFCryptocurrencyValuePredictor extends javax.swing.JFrame implement
         lblLiability2 = new javax.swing.JLabel();
         lblCopyright = new javax.swing.JLabel();
         lblDistribution = new javax.swing.JLabel();
+        lblContactDetails = new javax.swing.JLabel();
+        lblContactDetails2 = new javax.swing.JLabel();
+        lblContactDetails3 = new javax.swing.JLabel();
+        lblContactDetails4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMaximumSize(new java.awt.Dimension(800, 600));
@@ -191,6 +201,39 @@ public class JFCryptocurrencyValuePredictor extends javax.swing.JFrame implement
         lblDistribution.setFont(new java.awt.Font("SimSun", 0, 11)); // NOI18N
         lblDistribution.setText("Before using, modifying or distributing this software written authorisation from the developer is required.");
 
+        lblContactDetails.setFont(new java.awt.Font("SimSun", 0, 11)); // NOI18N
+        lblContactDetails.setText("Contact Details:");
+
+        lblContactDetails2.setFont(new java.awt.Font("SimSun", 0, 11)); // NOI18N
+        lblContactDetails2.setForeground(new java.awt.Color(0, 0, 255));
+        lblContactDetails2.setText("j.kellaway88@gmail.com");
+        lblContactDetails2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        lblContactDetails2.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                lblContactDetails2MouseDragged(evt);
+            }
+        });
+
+        lblContactDetails3.setFont(new java.awt.Font("SimSun", 0, 11)); // NOI18N
+        lblContactDetails3.setForeground(new java.awt.Color(0, 0, 255));
+        lblContactDetails3.setText("https://github.com/Arwedda");
+        lblContactDetails3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        lblContactDetails3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblContactDetails3MouseClicked(evt);
+            }
+        });
+
+        lblContactDetails4.setFont(new java.awt.Font("SimSun", 0, 11)); // NOI18N
+        lblContactDetails4.setForeground(new java.awt.Color(0, 0, 255));
+        lblContactDetails4.setText("https://www.linkedin.com/in/joseph-kellaway-604a22128/");
+        lblContactDetails4.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        lblContactDetails4.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblContactDetails4MouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout jpnlAboutLayout = new javax.swing.GroupLayout(jpnlAbout);
         jpnlAbout.setLayout(jpnlAboutLayout);
         jpnlAboutLayout.setHorizontalGroup(
@@ -210,13 +253,25 @@ public class JFCryptocurrencyValuePredictor extends javax.swing.JFrame implement
                         .addComponent(lblTitle)
                         .addGap(262, 262, 262))))
             .addGroup(jpnlAboutLayout.createSequentialGroup()
-                .addGap(240, 240, 240)
-                .addComponent(lblCopyright)
+                .addGroup(jpnlAboutLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jpnlAboutLayout.createSequentialGroup()
+                        .addGap(240, 240, 240)
+                        .addComponent(lblCopyright))
+                    .addGroup(jpnlAboutLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(lblContactDetails)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lblContactDetails2))
+                    .addGroup(jpnlAboutLayout.createSequentialGroup()
+                        .addGap(113, 113, 113)
+                        .addGroup(jpnlAboutLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblContactDetails4)
+                            .addComponent(lblContactDetails3))))
                 .addGap(0, 0, Short.MAX_VALUE))
         );
         jpnlAboutLayout.setVerticalGroup(
             jpnlAboutLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jpnlAboutLayout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jpnlAboutLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(lblTitle)
                 .addGap(9, 9, 9)
@@ -227,7 +282,15 @@ public class JFCryptocurrencyValuePredictor extends javax.swing.JFrame implement
                 .addComponent(lblLiability2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(lblDistribution)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 335, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jpnlAboutLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblContactDetails)
+                    .addComponent(lblContactDetails2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblContactDetails3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lblContactDetails4)
+                .addGap(275, 275, 275)
                 .addComponent(lblCopyright))
         );
 
@@ -262,6 +325,38 @@ public class JFCryptocurrencyValuePredictor extends javax.swing.JFrame implement
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void lblContactDetails3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblContactDetails3MouseClicked
+        openWebpage(lblContactDetails3.getText());
+    }//GEN-LAST:event_lblContactDetails3MouseClicked
+
+    private void lblContactDetails4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblContactDetails4MouseClicked
+        openWebpage(lblContactDetails4.getText());
+    }//GEN-LAST:event_lblContactDetails4MouseClicked
+
+    private void lblContactDetails2MouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblContactDetails2MouseDragged
+        writeEmail(lblContactDetails2.getText());
+    }//GEN-LAST:event_lblContactDetails2MouseDragged
+
+    private void openWebpage(String url) {
+        if (Desktop.isDesktopSupported()) {
+            try {
+                Desktop.getDesktop().browse(new URI(url));
+            } catch (Exception e) {
+                System.out.println("[INFO] Error: " + e);
+            }
+        }
+    }
+    
+    private void writeEmail(String address) {
+        if (Desktop.isDesktopSupported()) {
+            try {
+                Desktop.getDesktop().mail( new URI( "mailto:" + address));
+            } catch (Exception e) {
+                System.out.println("[INFO] Error: " + e);
+            }
+        }
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -306,6 +401,10 @@ public class JFCryptocurrencyValuePredictor extends javax.swing.JFrame implement
     private javax.swing.JPanel jpnlTrading;
     private javax.swing.JScrollPane jspValues;
     private javax.swing.JTable jtblValues;
+    private javax.swing.JLabel lblContactDetails;
+    private javax.swing.JLabel lblContactDetails2;
+    private javax.swing.JLabel lblContactDetails3;
+    private javax.swing.JLabel lblContactDetails4;
     private javax.swing.JLabel lblCopyright;
     private javax.swing.JLabel lblDeveloper;
     private javax.swing.JLabel lblDistribution;
