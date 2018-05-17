d163df36184100d08bd692f25df35ee36699e671
Joseph Kellaway <joseph.kellaway@students.plymouth.ac.uk>
2018-05-17 16:05:46 +0100

    Starts trading immediately.

--- CryptocurrencyValuePredictor/CryptocurrencyValuePredictorLibrary/src/main/java/com/jkellaway/cryptocurrencyvaluepredictorlibrary/valuepredictor/CryptocurrencyValuePredictor.java
+++ CryptocurrencyValuePredictor/CryptocurrencyValuePredictorLibrary/src/main/java/com/jkellaway/cryptocurrencyvaluepredictorlibrary/valuepredictor/CryptocurrencyValuePredictor.java
@@ -197,6 +197,8 @@ public class CryptocurrencyValuePredictor implements IObserver, ISubject {
         HoldMode holdMode =  (holdUSD ? HoldMode.USD : HoldMode.CRYPTOCURRENCY);
         //System.out.println(apiKey);
         trader = new Trader("USD", startValue, tradeMode, tradeModeNum, holdMode);
+        trader.autoTrade(getCurrencies());
+        notifyObservers();
     }
     
     /**
